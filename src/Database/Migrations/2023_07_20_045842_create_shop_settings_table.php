270e2b623ae567ea05352d9fef3c092adbd2c51b
AlexAndweb <56642732+AlexAndweb@users.noreply.github.com>
2023-08-02 19:38:12 +0300

    Update primary key

--- src/Database/Migrations/2023_07_20_045842_create_shop_settings_table.php
+++ src/Database/Migrations/2023_07_20_045842_create_shop_settings_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('shop_settings', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements();
             $table->string('tva', 255);
             $table->enum('tax_type', ['percent', 'fixed']);
             $table->boolean('maintenence');
