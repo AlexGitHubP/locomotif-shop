d6d9fc85bd21440f94e0867ab4301dc84a401227
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2023-08-17 16:31:37 +0300

    Add reseller invoices blueprint.

---
+++ src/Database/Migrations/2023_08_17_150545_create_reseller_invoices_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reseller_invoices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('designer_id')->index();
+            $table->decimal('subtotal_sales', 10, 2)->default(0);
+            $table->decimal('amount_to_invoice', 10, 2)->default(0);
+            $table->decimal('amount_shown_to_shop', 10, 2)->default(0);
+            $table->enum('invoice_status', ['notUploaded', 'uploaded', 'inProcesare', 'inregistrata', 'incasata']);
+            $table->string('invoice', 255)->nullable();
+            $table->unsignedBigInteger('nr_of_notice_sent')->default(0);
+            $table->integer('month');
+            $table->timestamps();
+
+            $table->foreign('designer_id')->references('id')->on('accounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reseller_invoices');
+    }
+};
